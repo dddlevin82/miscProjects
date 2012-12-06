c0db2f442112428937b6b22224c0dca8358022eb
Daniel Reid <dreid1991@gmail.com>
2012-12-05 22:55:53 -0800

    section dragging works

--- tree/tree.js
+++ tree/tree.js
@@ -3,15 +3,20 @@ function Tree(paper, pos) {
 	this.pos = pos; //upper left corner of first section
 	this.blockHeight = 40;
 	this.blockWidth = 100;
+	this.circleOffset = V(80, 0);
+	this.circleRad = 15;
 	this.blockSpacing = 10;
 	this.totalBlockHeight = this.blockHeight + this.blockSpacing;
 	this.promptIndent = 30;
-	this.blockCol = Col(100,160,193);//'#64a0c1';
+	this.blockCol = Col(100, 160, 193);//'#64a0c1';
 	this.blockColHover = Col(92, 147, 178);//'#5c93b2';
-	this.blockColSelect = Col(82,108,122);//'#526c7a';
-	this.blockColStroke = Col(59,68,73);//'#3b4449';
-	this.placerRectPos = P(200, 300);
-	this.placerRect = this.makePlacerRect();
+	this.blockColSelect = Col(82, 108, 122);//'#526c7a';
+	this.blockColStroke = Col(59, 68, 73);//'#3b4449';
+	this.circleCol = Col(120, 180, 213);
+	this.placerButtonPos = P(200, 300);
+	this.defineSectionDragFuncs();
+	this.placerButton = this.makePlacerButton();
+	
 	//this.placerBGRect = this.makePlacerBGRect();
 	this.sections = [];
 	
@@ -23,7 +28,7 @@ Tree.prototype = {
 		var sectionIdx = this.getNewSectionIdx(pos);
 		if (!section) {
 		/*should make it send rectangle corner positions*/
-			section = new TreeSection(this, pos/*GET A POINT FOR THE UPPER LEFT CORNER, NOT FOR MOUSEPOS*/, undefined, undefined);
+			section = new TreeSection(this, pos/*GET A POINT FOR THE UPPER LEFT CORNER, NOT FOR MOUSEPOS*/, this.sectionDragFuncs, undefined);
 		}
 		this.sections.splice(sectionIdx, 0, section);
 		//for (var idx=sectionIdx+1; idx<this.sections.length; idx++) {
@@ -79,9 +84,87 @@ Tree.prototype = {
 		}
 		return this.sections.length-1;
 	},
-	makePlacerRect: function() {
-		var pos = this.placerRectPos;
+	makePlacerButton: function() {
+		var pos = this.placerButtonPos;
 		var placer = new TreeSection(this, pos, undefined, undefined);
+		return placer;
+	},
+	getSectionIdx: function(section) {
+		return this.sections.indexOf(section);
+	},
+	//drag and click functions are in context of the rect or circle.  this.parent will reference the button object.
+	sectionDragStart: function() {
+		this.parent.tree.clickedButton = this.parent;
+		this.parent.sectionIdx = this.parent.tree.getSectionIdx(this.parent.parent);
+		this.parent.mousePos = posOnPaper(globalMousePos, this.parent.tree.paper);
+		this.parent.sectionYs = this.parent.tree.getSectionYs();
+	},
+	sectionDragMove: function() {
+		var curSectionIdx = this.parent.sectionIdx;
+		var mousePos = posOnPaper(globalMousePos, this.parent.tree.paper);
+		var dPos = V(mousePos.x - this.parent.mousePos.x, mousePos.y- this.parent.mousePos.y);
+		this.parent.parent.move(dPos);
+		var pos = this.parent.pos;
+		var sectionHeight = this.parent.parent.totalHeight();
+		this.parent.mousePos.set(mousePos);
+		var sectionYs = this.parent.sectionYs;
+		for (var sectionIdx=0; sectionIdx<sectionYs.length; sectionIdx++) {
+			var midPtY = sectionYs[sectionIdx] + this.parent.tree.sections[sectionIdx].totalHeight()/2;
+			if (sectionIdx < curSectionIdx) {
+				if (pos.y < midPtY) {
+					var newSectionIdx = sectionIdx;
+					break;
+				}
+			} else if (sectionIdx > curSectionIdx) {
+				if (pos.y + sectionHeight > midPtY) {
+					var newSectionIdx = sectionIdx;
+					break;
+				}
+			}
+		}
+		
+		//if (nextSectionIdx === undefined) {
+		//	nextSectionIdx = curSectionIdx;
+		//}
+		if (newSectionIdx !== undefined) {//(nextSectionIdx != this.parent.sectionIdx) {
+			var a = this.parent.tree.sections[this.parent.sectionIdx];
+			var b = this.parent.tree.sections[newSectionIdx];
+			this.parent.tree.sections[this.parent.sectionIdx] = b;
+			this.parent.tree.sections[newSectionIdx] = a;
+			this.parent.sectionIdx = newSectionIdx;
+			this.parent.tree.moveAllToPositions('fly');
+		}
+		
+	},
+	sectionDragEnd: function() {
+		this.parent.tree.clickedButton = undefined;
+		this.parent.sectionIdx = undefined;
+		this.parent.mousePos = P(0, 0);
+		this.parent.sectionYs = [];
+		this.parent.tree.moveAllToPositions('fly');
+	},
+	defineSectionDragFuncs: function() {
+		this.sectionDragFuncs = {
+			onStart: this.sectionDragStart,
+			onMove: this.sectionDragMove,
+			onEnd: this.sectionDragEnd
+		}
+	},
+	definePromptDragFuncs: function() {
+		this.promptDragFuncs = {
+			onStart: this.promptDragStart,
+			onMove: this.promptDragMove,
+			onEnd: this.promptDragEnd		
+		}
+	},
+	getSectionYs: function() {
+		var y = this.pos.y;
+		var ys = [];
+		for (var sectionIdx=0; sectionIdx<this.sections.length; sectionIdx++) {
+			ys.push(y);
+			y += this.totalBlockHeight * (1 + this.sections[sectionIdx].prompts.length);
+		}
+		return ys;
 	},
 	moveAllToPositions: function(moveStyle) {
 		var x = this.pos.x;
@@ -108,6 +191,8 @@ Tree.prototype = {
 						y += this.totalBlockHeight;
 					}
 				}
+			} else {
+				y += this.sections[sectionIdx].totalHeight();
 			}
 		}
 	},
@@ -118,7 +203,10 @@ function TreeSection(tree, posInit, dragFuncs, onClick) {
 	this.tree = tree;
 	this.prompts = [];
 	this.pos = posInit.copy();
-	this.button = new TreeButton(this.tree, this.pos, dragFuncs, onClick);
+	this.initSectionIdx = undefined; //for dragging
+	this.mousePosInit = P(0, 0); //for dragging
+	this.sectionYs = []; //for dragging
+	this.button = new TreeButton(this.tree, this, this.pos, dragFuncs, onClick);
  
 }
 
@@ -129,6 +217,13 @@ TreeSection.prototype = {
 			prompt = new TreePrompt(this.tree, this, releasePos)
 		}
 		this.prompts.splice(newIdx, 0, prompt);
+	},
+	move: function(v) {
+		this.pos.movePt(v);
+		this.button.move(v);
+		for (var promptIdx=0; promptIdx<this.prompts.length; promptIdx++) {
+			this.prompts[promptIdx].move(v);
+		}
 	},
 	getNewPromptIdx: function(releasePos) {
 		var y = this.pos.y + this.tree.totalBlockHeight;
@@ -161,10 +256,10 @@ TreeSection.prototype = {
 	}
 }
 
-function TreePrompt(tree, section, posInit) {
+function TreePrompt(tree, section, posInit, dragFuncs, onClick) {
 	this.tree = tree;
 	this.section = section;
-	this.button = new TreeButton(this.tree, posInit, undefined, undefined);
+	this.button = new TreeButton(this.tree, this, posInit, dragFuncs, undefined);
 }
 
 TreePrompt.prototype = {
@@ -176,13 +271,20 @@ TreePrompt.prototype = {
 	},
 	remove: function() {
 		this.button.remove();
+	},
+	move: function(v) {
+		this.button.move(v);
 	}
 }
 
-function TreeButton(tree, posInit, dragFuncs, onClick) {
+function TreeButton(tree, parent, posInit, dragFuncs, onClick) {
 	this.tree = tree;
 	this.pos = posInit.copy();
+	this.dragFuncs = dragFuncs;
+	this.parent = parent;
 	this.rect = this.makeRect();
+	this.circle = this.makeCircle();
+	
 }
 
 TreeButton.prototype = {
@@ -197,21 +299,65 @@ TreeButton.prototype = {
 			'stroke-linejoin': 'round',
 			}
 		);
-		rect.transform('t' + this.pos.x + ',' + this.pos.y);
+		if (this.dragFuncs) {
+			rect.drag(this.dragFuncs.onMove, this.dragFuncs.onStart, this.dragFuncs.onEnd);
+		}
+		var pos = this.rectPos();
+		rect.transform('t' + pos.x + ',' + pos.y);
+		rect.parent = this;
 		return rect;
 	},
-	
+	setParent: function(parent) {
+		this.parent = parent;
+	},
+	makeCircle: function() {
+		var circle = paper.circle(0, 0, this.tree.circleRad);
+		circle.attr(
+			{
+			fill: this.tree.circleCol,
+			
+			}
+		);
+		if (this.dragFuncs) {
+			circle.drag(this.dragFuncs.onMove, this.dragFuncs.onStart, this.dragFuncs.onEnd);
+		}
+		var pos = this.circlePos();
+		circle.transform('t' + pos.x + ',' + pos.y);
+		circle.parent = this;
+		return circle;
+	},	
+	move: function(v) {
+		var pos = P(this.pos.x + v.dx, this.pos.y + v.dy);
+		this.snapToPos(pos);
+	},
 	snapToPos: function(pos) {
 		if (!pos.sameAs(this.pos)) {
-			this.rect.transform('t' + pos.x + ',' + pos.y);
+			this.pos.set(pos);
+			var rectPos = this.rectPos();
+			var circlePos = this.circlePos();
+			this.rect.toFront();
+			this.circle.toFront();
+			this.rect.transform('t' + rectPos.x + ',' + rectPos.y);
+			this.circle.transform('t' + circlePos.x + ',' + circlePos.y);
 		}
-		this.pos.set(pos);	
+			
 	},
 	flyToPos: function(pos) {
 		if (!pos.sameAs(this.pos)) {
-			this.rect.animate({transform:'t' + pos.x + ',' + pos.y}, 250);
+			this.pos.set(pos);
+			var rectPos = this.rectPos();
+			var circlePos = this.circlePos();
+			this.rect.toFront();
+			this.circle.toFront();
+			this.rect.animate({transform:'t' + rectPos.x + ',' + rectPos.y}, 250);
+			this.circle.animate({transform:'t' + circlePos.x + ',' + circlePos.y}, 250);
 		}
-		this.pos.set(pos);
+	},
+	rectPos: function() {
+		return this.pos.copy();
+	},
+	circlePos: function() {
+		return P(this.pos.x + this.tree.circleOffset.dx, this.pos.y + this.tree.circleOffset.dy + this.tree.blockHeight/2);
 	},
 	remove: function() {
 		this.rect.remove();
