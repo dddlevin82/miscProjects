6885268a1a51ff37ee694dbea7b5ade4f14ee7f0
Daniel Reid <dreid1991@gmail.com>
2012-12-29 21:19:42 -0800

    interpreting blips and bloops correctly

--- morseCode/listener.js
+++ morseCode/listener.js
@@ -1,100 +1,245 @@
 //0 when on
 //1 when off
-var tokenStack = [];
-var letterStack = [];
-var string = '';
-var timeStart;
-var lastValue = 1;
-var unitLen = 300; //ms
-var medLen = 3*unitLen;
-var longLen = 7*unitLen;
-var dashTime = 
-var gpio = require('pi-gpio');
+var repl = require('repl');
+var unitTime = 300; //ms
+var extraPenalty = 1;
+var switchingPenalty = 3;
+//var gpio = require('pi-gpio');
 
-var timeToTokenMap = 
+var tokenToTimeMap = 
 {
 	//corresponds to value it was just on, not new value
-	'0': 
-	{
-		'unit': '.',
-		'med': '-',
-		'long': '-'
-	},
-	'1': 
-	{
-		'unit': '',
-		'med': '|', //between letters
-		'long': '^' //between words
-	}
+	'.': function() {return unitTime},
+	'-': function() {return 3 * unitTime},
+	'|': function() {return unitTime},
+	'/': function() {return 3 * unitTime},
+	'^': function() {return 4 * unitTime}
 }
 
-var tokenToLetterMap = 
+var tokenIsDown = 
 {
-	'.-': 	'A',
-	'-...':	'B',
-	'-.-.':	'C',
-	'-..': 	'D',
-	'.': 	'E',
-	'..-.':	'F',
-	'--.': 	'G',
-	'....':	'H',
-	'..': 	'I',
-	'.---':	'J',
-	'-.-': 	'K',
-	'.-..': 'L',
-	'--':	'M',
-	'-.':	'N',
-	'---':	'O',
-	'.--.':	'P',
-	'--.-':	'Q',
-	'.-.':	'R',
-	'...':	'S',
-	'-':	'T',
-	'..-':	'U',
-	'...-':	'V',
-	'.--':	'W',
-	'-..-':	'X',
-	'-.--':	'Y',
-	'--..':	'Z'
+	'.': 1,
+	'-': 1,
+	'|': 0,
+	'/': 0,
+	'^': 0
 }
-	
 
-gpio.open(16, 'input', function(){});
+function Interpretation(string, wrongness) {
+	this.string = string;
+	this.wrongness = wrongness;
+}
+
+function Blip(isDown, time) {
+	this.down = isDown;
+	this.time = time;
+}
+
+Blip.prototype = {
+	wrongness: function(token) {
+		var wrongness = Math.abs(this.down - tokenIsDown[token]) * switchingPenalty;
+		if (token == '^') {
+			return wrongness + Math.max(0, tokenToTimeMap[token]() - this.time)/unitTime;
+		}
+		return wrongness + Math.abs(tokenToTimeMap[token]() - this.time)/unitTime;
+	},
+}
+
+var letterToTokensMap = 
+{
+	'A': '.|-/'			,
+	'B': '-|.|.|./'		,
+	'C': '-|.|-|./'		,		
+	'D': '-|.|./' 		,
+	'E': './'			,
+	'F': '.|.|-|./'		,
+	'G': '-|-|./'		,
+	'H': '.|.|.|./'		,
+	'I': '.|./'			,
+	'J': '.|-|-|-/'		,
+	'K': '-|.|-/' 		,
+	'L': '.|-|.|./' 	,
+	'M': '-|-/'			,
+	'N': '-|./'			,
+	'O': '-|-|-/'		,
+	'P': '.|-|-|./'		,
+	'Q': '-|-|.|-/'		,
+	'R': '.|-|./'		,
+	'S': '.|.|./'		,
+	'T': '-/'			,
+	'U': '.|.|-/'		,
+	'V': '.|.|.|-/'		,
+	'W': '.|-|-/'		,
+	'X': '-|.|.|-/'		,
+	'Y': '-|.|-|-/'		,
+	'Z': '-|-|.|./'		,
+	' ': '^'
+}
 
 
-function checkVal() {
-	gpio.read(16, function(err, value) {
-	
-	if (err) throw err;
-	
-	if (value != lastValue) {
-		var dTime = Date.now() - timeStart;
-		timeStart = Date.now()
-		appendTokenStack(dTime, value);
+
+//gpio.open(16, 'input', function(){});
+
+var interpreter = new Interpreter();
+
+
+function Listener() {
+	this.addedLetterEnd = false;
+	this.addedSpace = false;
+	this.timeStart = 0;
+	this.lastValue = 1;
+}
+
+Listener.prototype = {
+	listen: function() {
+		gpio.read(16, function(err, value) {
+			
+			if (err) throw err;
+			
+			var dTime = Date.now() - this.timeStart;
+			if (value == 1 && dTime > 5 * unitTime) {
+				//making it add a letter end character if it's been up for more than halfway to letter end + space
+				//this will also add more spaces as you wait
+				if (!this.addedLetterEnd) {
+					interpreter.addBlip(value, 3.5 * unitTime);
+					this.timeStart = Date.now() - 1.5 * unitTime; 
+					this.addedLetterEnd = true;
+				} else if (!this.addedSpace && dTime > 7 * unitTime) {
+					interpreter.addBlip(value, 7 * unitTime);
+					this.timeStart = Date.now();
+					this.addedSpace = true;
+				}
+			}
+			if (value != this.lastValue) {
+				if (!this.addedLetterEnd && !this.addedSpace) {
+					this.timeStart = Date.now()
+					interpreter.addBlip(!value, dTime); // 0->down, 1->up, so have to flip
+				}
+				this.addedLetterEnd = false;
+				this.addedSpace = false;
+			}
+		})
 	}
 }
 
-function getNewToken(dTime, value) {
-	var type;
-	if (dTime < avg(unitLen, medLen)) {
-		type = 'unit';
-	} else if (dTime < avg(medLen, longLen)) {
-		type = 'med';
-	} else {
-		type = 'long';
-	}
-	return timeToTokenMap[value][Number(!type)]; //to send what it was just on, not new value
+//should do space as seperate character
+
+function Interpreter() {
+	this.interps = [];
+	this.blips = [];
 }
 
-function appendTokenStack(dTime, value) {
-	var letter;
-	var newChar = getNewToken(dTime, value);
-	tokenStack.push(newChar);
-	if (newChar == '^' || newChar == '|') {
-		letter = getNewLetter(tokenStack);
+Interpreter.prototype = {
+	getLastStr: function() {
+		return this.interps[this.interps.length-1].string;
+	},
+	print: function() {
+		console.log(this.interps);
+		console.log(this.interps[this.interps.length-1].string);
+		console.log(this.interps[this.interps.length-1].wrongness);
+	},
+	addBlip: function(value, dTime) {
+		this.blips.push(new Blip(value, dTime));
+		this.addInterpretation();
+	},
+	addInterpretation: function() {
+		var idx = Math.max(0, this.blips.length - 10);
+		var nextInterps = [];
+		for (; idx<this.blips.length; idx++) {
+			var minWrongness = Number.MAX_VALUE;
+			var bestStr = '';
+			var prevInterp = this.interps[idx-1];
+			if (!prevInterp) {
+				var prevWrongness = 0;
+				var prevStr = '';
+			} else {
+				var prevWrongness = prevInterp.wrongness;
+				var prevStr = prevInterp.string;
+			}
+			for (var letter in letterToTokensMap) {
+				
+				var wrongness = this.evalWrongness(this.blips.slice(idx, this.blips.length), letter);
+			
+				if (wrongness + prevWrongness < minWrongness) {
+					minWrongness = wrongness + prevWrongness;
+					bestStr = prevStr + letter;
+				}
+			}
+
+			nextInterps.push(new Interpretation(bestStr, minWrongness));
+		
+		}
+
+		var minWrongness = Number.MAX_VALUE;
+		var bestStr = '';
+		var bestInterp;
+		for (var idx=0; idx<nextInterps.length; idx++) {
+			if (nextInterps[idx].wrongness < minWrongness) {
+				bestInterp = nextInterps[idx];
+				minWrongness = nextInterps[idx].wrongness;
+			}
+		}
+		this.interps.push(bestInterp);	
+	},
+	evalWrongness: function(blips, string) {
+		var wrongness = 0;
+		var blipIdx = 0;
+		for (var letterIdx=0; letterIdx<string.length; letterIdx++) {
+			var letter = string[letterIdx];
+			var tokens = letterToTokensMap[letter.toUpperCase()];
+		
+			for (var tokenIdx=0; tokenIdx<tokens.length; tokenIdx++) {
+				if (blipIdx == blips.length) {
+					return wrongness + (this.numTokensInStr(string) - blipIdx) * extraPenalty; 
+				}
+				var blip = blips[blipIdx];
+				wrongness += blip.wrongness(tokens[tokenIdx])
+				blipIdx++;
+			}
+				
+		}
+		return wrongness + (blips.length - blipIdx) * extraPenalty;	
+	},
+	numTokensInStr: function(str) {
+		var numTokens = 0;
+		for (var idx=0; idx<str.length; idx++) {
+			numTokens += letterToTokensMap[str[idx].toUpperCase()].length;
+		}
+		return numTokens;
 	}
 }
 
-function avg(a, b) {
-	return (a + b) / 2;
+function myEval(cmd, context, filename, callback) {
+	try {
+		callback(null, eval(cmd));
+	} catch (e) {
+		callback(e);
+	}
 }
+
+function testStr(str) {
+	var interpreter = new Interpreter();
+
+	var tokens = '';
+	for (var strIdx=0; strIdx<str.length; strIdx++) {
+		tokens += letterToTokensMap[str[strIdx].toUpperCase()];
+	}
+	for (var tokenIdx=0; tokenIdx<tokens.length; tokenIdx++) {
+		var token = tokens[tokenIdx];
+		if (token == '.' || token == '-') {
+			interpreter.addBlip(1, tokenToTimeMap[token]()*(1 + Math.random()*.2-.1));
+		} else if (token == '^' ) {
+			interpreter.addBlip(0, tokenToTimeMap[token]()*.9);
+		} else {
+			interpreter.addBlip(0, tokenToTimeMap[token]());
+		}
+
+	}
+	interpreter.print();
+
+	console.log(str);
+}
+
+testStr('e  e  batf');
+
+repl.start({eval: myEval});
